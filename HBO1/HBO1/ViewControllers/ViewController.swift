2d205d1a79ad33a4710991102142eb744cceafbc
Kavindu0096 <kavindnimasara@gmail.com>
2020-01-14 02:10:34 -0800

    FirstCommit

---
+++ HBO1/HBO1/ViewControllers/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  HBO1
+//
+//  Created by Kavindu Nimsara on 1/13/20.
+//  Copyright © 2020 Kavindu Nimsara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var pwText: UITextField!
+    @IBOutlet weak var signInBtn: UIButton!
+    @IBOutlet weak var emailTextView: UIView!
+    
+    @IBOutlet weak var pwTextView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emailTextView.layer.cornerRadius=10;
+        emailTextView.clipsToBounds=true;
+        pwTextView.layer.cornerRadius=10;
+        pwTextView.clipsToBounds=true;
+        
+    }
+
+
+}
+
